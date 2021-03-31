3e2ab813452832e58fef6b4ea334b87466a23be2
John Meyer <af4jm@users.noreply.github.com>
2021-03-31 19:52:34 -0400

    removed mucking of filenames & labels... if you want a dash you put a dash, if you put a space you want a space

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,5 @@
 'use strict'
 const vscode = require('vscode')
-const _ = require('lodash')
-const sanitize = require('sanitize-filename')
  
 function PageNameGenerator(label) {
 
@@ -10,15 +8,7 @@ function PageNameGenerator(label) {
     return label.split('/').map(function(pathSegment) {
 
         // remove file path extension
-        pathSegment = pathSegment.replace(/\.[^/.]+$/, "");
-
-        // clean up unwanted characters and replace with a -
-        pathSegment = _.deburr(pathSegment);
-        pathSegment = pathSegment.replace(/[^\w\s]/g, '-');
-
-        // slugify
-        pathSegment = pathSegment.split(" ").join("-");
-        return pathSegment.toLowerCase();
+        return pathSegment.replace(/\.[^/.]+$/, "");
       
     }).join('/');
 }
@@ -27,29 +17,14 @@ function postProcessPageName(pageName) {
 
     console.log(`postprocess: ${pageName}`)
 
-    pageName = pageName.trim();
-
-	// Sanitize filenames and slugify
-    pageName = pageName.split('/').map(sanitize).join('/');
-    pageName = pageName.split(/\s+/).join('-');
-    return pageName;
-
+    return pageName.trim();
 }
   
 function postProcessLabel(label) {
     console.log(`postprocesslabel : ${label}`)
 
-
-	label = label.trim();
+    label = label.trim();
 	
-	// Remove filename extension
-	// label = label.replace(/\.[^/.]+$/, "");
-	
-	// De-slugify and add matching brackets
-    label = label.replace(/(?<!\\)-/, " ");
-    label = label.replace(/\\-/, "-");
-
-    // label = label.split("-").join(" ");
     if (vscode.workspace.getConfiguration("markdown-wiki-links-preview").get('showextension')) {
         label += vscode.workspace.getConfiguration("markdown-wiki-links-preview").get('urisuffix');
     }
@@ -82,8 +57,8 @@ function activate(context) {
         }
     };
 }
+
 exports.activate = activate;
 exports.PageNameGenerator = PageNameGenerator;
 exports.postProcessPageName = postProcessPageName;
 exports.postProcessLabel = postProcessLabel;
-
