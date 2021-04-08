3e475cd907a42ea63d19e3b6a2e1f11a18b8a055
John Meyer, AF4JM <af4jm@users.noreply.github.com>
2021-04-07 22:44:51 -0400

    bug fix

--- src/extension.js
+++ src/extension.js
@@ -1,6 +1,6 @@
 'use strict'
 const vscode = require('vscode')
- 
+
 function PageNameGenerator(label) {
 
     console.log(`PageNameGen: ${label}`)
@@ -11,10 +11,10 @@ function PageNameGenerator(label) {
         pathSegment = pathSegment.replace(/\.[^/.]+$/, "");
 
         // escape spaces in the filename
-	pathSegment = pathSegment.replace(/ /g, '%20');
+        pathSegment = pathSegment.replace(/ /g, '%20');
 
         return pathSegment;
-      
+
     }).join('/');
 }
 
@@ -22,14 +22,14 @@ function postProcessPageName(pageName) {
 
     console.log(`postprocess: ${pageName}`)
 
-    return pageName.trim();
+    return pageName.replace(/\|/g, '').trim();
 }
-  
+
 function postProcessLabel(label) {
     console.log(`postprocesslabel : ${label}`)
 
     label = label.trim();
-	
+
     if (vscode.workspace.getConfiguration("markdown-wiki-links-preview").get('showextension')) {
         label += vscode.workspace.getConfiguration("markdown-wiki-links-preview").get('urisuffix');
     }
@@ -50,10 +50,10 @@ function activate(context) {
     return {
         extendMarkdownIt(md) {
             return md.use(
-                
-                require('@thomaskoppelaar/markdown-it-wikilinks')({ 
-                    generatePageNameFromLabel: PageNameGenerator, 
-                    postProcessPageName: postProcessPageName, 
+
+                require('@thomaskoppelaar/markdown-it-wikilinks')({
+                    generatePageNameFromLabel: PageNameGenerator,
+                    postProcessPageName: postProcessPageName,
                     postProcessLabel: postProcessLabel,
                     uriSuffix: `${vscode.workspace.getConfiguration("markdown-wiki-links-preview").get('urisuffix')}`,
                     description_then_file: vscode.workspace.getConfiguration("markdown-wiki-links-preview").get("descriptionthenfile"),
